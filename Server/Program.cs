3e621ae90be43fa0dde6cee506a55089173b7ef4
Corey Kruzicki <corey0708@gmail.com>
2024-02-11 17:01:00 -0600

    Added BFF Logic/classes. Added Serilog/Sinks/Seq Setup tempary config to access IDP.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,36 +1,125 @@
-using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add services to the container.
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseWebAssemblyDebugging();
+    Log.Information("Starting TW Wasm Server host");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .MinimumLevel.Information()
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+        .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+        .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+        .MinimumLevel.Override("IdentityModel", LogEventLevel.Debug)
+        .MinimumLevel.Override("Duende.Bff", LogEventLevel.Debug)
+        .Enrich.FromLogContext()
+        .WriteTo.Seq("http://localhost:5341")
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",theme: AnsiConsoleTheme.Literate));
+
+    //builder.Services.AddControllers();
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddRazorPages();
+    builder.Services.AddBff();
+
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        options.DefaultSignOutScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    }).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+      .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+        {
+            options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.Authority = builder.Configuration["Authentication:Authority"];
+
+            // confidential client using code flow + PKCE
+            options.ClientId = "twclient";
+            options.ClientSecret = builder.Configuration["ClientSecret"];
+            options.ResponseType = "code";
+            options.ResponseMode = "query";
+
+            options.ClaimActions.MapUniqueJsonKey("address", "address");
+            options.TokenValidationParameters.NameClaimType = "address";
+
+            options.MapInboundClaims = false;
+            options.SaveTokens = true;
+            options.GetClaimsFromUserInfoEndpoint = true;
+            options.ClaimActions.Remove("aud"); // Removes filter, makes sure it stays
+            options.ClaimActions.DeleteClaim("sid");
+            options.ClaimActions.DeleteClaim("idp");
+
+            options.Scope.Clear();
+            options.Scope.Add("openid");
+            options.Scope.Add("profile");
+            options.Scope.Add("tw-api.fullaccess");
+            options.Scope.Add("country");
+            options.Scope.Add("fullname");
+            options.Scope.Add("offline_access");
+            options.ClaimActions.MapJsonKey("role", "role");
+            options.ClaimActions.MapJsonKey("fullname", "fullname");
+            options.ClaimActions.MapUniqueJsonKey("country", "country");
+
+            options.TokenValidationParameters = new()
+            {
+                NameClaimType = "given_name",
+                RoleClaimType = "role",
+            };
+        });
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseWebAssemblyDebugging();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseBlazorFrameworkFiles();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+    app.UseAuthentication();
+    app.UseBff();
+    app.UseAuthorization();
+
+    app.MapBffManagementEndpoints();
+
+    app.MapRazorPages();
+
+    app.MapControllers()
+        .RequireAuthorization()
+        .AsBffApiEndpoint();
+
+    app.MapFallbackToFile("index.html");
+
+    app.Run();
 }
-else
+catch (Exception ex)
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Fatal(ex, "TW Server Wasm Host terminated unexpectedly");
 }
-
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-app.Run();
+finally
+{
+    Log.Information("Shut down of TW Server Wasm Host complete");
+    Log.CloseAndFlush();
+}
