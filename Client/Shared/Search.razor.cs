e84cc57d8ed200c7621dab76ac7853c87681c8b1
Corey Kruzicki <corey0708@gmail.com>
2024-03-21 16:15:20 -0500

    Added more missed files.

--- Client/Shared/Search.razor.cs
+++ Client/Shared/Search.razor.cs
@@ -1,6 +1,18 @@
-﻿namespace TanglewoodCandleCo.Wasm.Client.Shared
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TanglewoodCandleCo.Wasm.Client.Shared
 {
     public partial class Search
     {
+        [Parameter]
+        public EventCallback<string> GlobalCallback { get; set; }
+        
+        [Parameter]
+        public List<object>? Arguments { get; set; }
+
+        public async Task GlobalSearch(string value)
+        {
+            await GlobalCallback.InvokeAsync(value);
+        }
     }
 }
